1fdaac715de35dfce57a0d43704444bebe87ef40
Mário Pina <86622495+mfopina@users.noreply.github.com>
2024-09-30 14:28:03 -0300

    Add files via upload

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -6,6 +6,17 @@ async function vizualizarInformacaoGlobais(){
 const res = await fetch(url)
 const dados = await res.json()
 console.log(dados);
+
+const paragrafo = document.createElement('p')
+paragrafo.classList.add('graficos-container__texto')
+
+paragrafo.innerHTML = `Você sabia que o mundo tem 
+${dados.total_pessoas_conectadas}`
+
+console.log(paragrafo)
+
+const container = document.getElementById("graficos-container")
+container.appendChild(paragrafo)
 }
 
 vizualizarInformacaoGlobais();
